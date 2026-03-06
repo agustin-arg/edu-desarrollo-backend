d50d782e56e006638721a993c1983f5bac535fd1
agustin-arg <agustinalbertoleiva@gmail.com>
2026-03-06 07:43:33 -0300

    feat: - update home template of flask course to include delete option for notes - add CSS class file 2 through 10

--- curso-flask/app.py
+++ curso-flask/app.py
@@ -118,3 +118,14 @@ def edit_note(id):
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("edit_note.html", note=note)
+
+
+@app.route("/delete-note/<int:id>", methods=["POST"])
+def delete_note(id):
+    note = Note.query.get(id)
+    if not note:
+        return redirect(url_for("home"))
+    db.session.delete(note)
+    db.session.commit()
+
+    return redirect(url_for("home"))
