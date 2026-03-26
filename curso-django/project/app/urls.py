66719655600cc741ca4c085f383ce0035a5f567b
agustin-arg <agustinalbertoleiva@gmail.com>
2026-03-26 11:19:49 -0300

    feat: refactor views and add URL configuration for CarListView and test view

--- curso-django/project/app/urls.py
+++ curso-django/project/app/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path
-from app.views import my_view
+from django.http import HttpResponse
+from django.urls import path
+from .views import my_view, my_test_view, CarListView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("carros/", include("app.urls")),
+
+    path("listado/", CarListView.as_view()),
+    path("list/", my_test_view),
+    path("detail/<int:id>", my_test_view),
 ]
--- curso-django/project/app/views.py
+++ curso-django/project/app/views.py
@@ -1,9 +1,24 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Car
 
+
 def my_view(request):
     car_list = Car.objects.all()
-    context = {
-        "car_list": car_list
-    }
-    return render(request=request,template_name="app/car_list.html", context=context)
+    context = {"car_list": car_list}
+    return render(request=request, template_name="app/car_list.html", context=context)
+
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
+
+
+class CarListView(TemplateView):
+    template_name = "app/car_list.html"
+
+    def get_context_data(self, **kwargs):
+        car_list = Car.objects.all()
+        return {"car_list": car_list}
