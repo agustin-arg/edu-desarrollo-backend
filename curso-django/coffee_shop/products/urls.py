8b2732fd4e38cb1d4014566bf404eea22a1e7423
agustin-arg <agustinalbertoleiva@gmail.com>
2026-04-02 22:42:08 -0300

    feat: implement product management features with forms, views, and templates; add media handling and update URL configurations

--- curso-django/coffee_shop/products/urls.py
+++ curso-django/coffee_shop/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductFormView
+from .views import ProductFormView, ProductListView, ProductTableView
 
 urlpatterns = [
-    path("add/", ProductFormView.as_view()),
+    path("add/", ProductFormView.as_view(), name="add_product"),
+    path("table/", ProductTableView.as_view(), name="table_product"),
+    path("list/", ProductListView.as_view(), name="list_product"),
 ]
--- curso-django/coffee_shop/products/views.py
+++ curso-django/coffee_shop/products/views.py
@@ -1,7 +1,25 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductForm
+from .models import Product
 
 
 class ProductFormView(generic.FormView):
-    template_name = 'products/add_product.html'
+    template_name = "products/add_product.html"
     form_class = ProductForm
+    success_url = reverse_lazy("list_product")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class ProductTableView(generic.ListView):
+    model = Product
+    template_name = "products/table_product.html"
+    context_object_name = "products"
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = "products/list_product.html"
+    context_object_name = "products"
