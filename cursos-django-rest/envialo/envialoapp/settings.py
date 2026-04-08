547a9040a870ce9c49300a0167d8a93aae1cf1e9
agustin-arg <agustinalbertoleiva@gmail.com>
2026-04-08 10:31:07 -0300

    feat: add initial structure for campaign, company, contact, customers, recipient, and users apps with basic files

--- cursos-django-rest/envialo/envialoapp/settings.py
+++ cursos-django-rest/envialo/envialoapp/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "campaign",
+    "company",
+    "contact",
+    "users",
+    "recipient",
 ]
 
 MIDDLEWARE = [
