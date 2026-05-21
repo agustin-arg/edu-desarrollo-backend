1b0af914cf0889cfe19434d06c30ab5100e5e0f7
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-21 20:55:39 -0300

    feat: add Basic and Session authentication classes to REST framework settings

--- curso-django-rest/convert_files/convert_files/settings.py
+++ curso-django-rest/convert_files/convert_files/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "converter",
     "users",
     'drf_spectacular',
@@ -122,4 +123,9 @@ STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
+
