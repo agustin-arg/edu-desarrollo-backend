3ee2f43bb64da7d3306833371e1da78679424e7a
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-20 23:37:14 -0300

    feat: add documentation app with API schema and Swagger UI integration

--- curso-django-rest/convert_files/convert_files/settings.py
+++ curso-django-rest/convert_files/convert_files/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "converter",
     "users",
+    'drf_spectacular',
+    "doc",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
--- curso-django-rest/convert_files/convert_files/urls.py
+++ curso-django-rest/convert_files/convert_files/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('doc.urls'),),
     path('api/', include('users.urls'),),
     path('api/', include('converter.urls'),)
 ]
