745d59ed893a56bc3782f15f8caef3591ad28525
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-22 08:21:09 -0300

    feat: add IsAuthenticated permission to UserViewSet

--- curso-django-rest/convert_files/users/views.py
+++ curso-django-rest/convert_files/users/views.py
@@ -7,12 +7,14 @@ from converter.serializers import FileSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 import rest_framework.status as status
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "id"
+    permission_classes = [IsAuthenticated]
 
     @action(detail=True, methods=["get"])
     def files(self, request, id=None):
