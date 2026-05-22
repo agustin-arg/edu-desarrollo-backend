4cf8fdf0f0f9764d475f4d03138a945be95d22b6
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-22 08:21:02 -0300

    feat: add API authentication URL to urlpatterns

--- curso-django-rest/convert_files/convert_files/urls.py
+++ curso-django-rest/convert_files/convert_files/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+import rest_framework
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('doc.urls'),),
     path('api/', include('users.urls'),),
-    path('api/', include('converter.urls'),)
+    path('api/', include('converter.urls'),),
+    path('api-auth', include('rest_framework.urls'))
 ]
